11e99a664a3a1130440b2c1060ba6fb0c20984e4
egorovin <egorovin@mail.ru>
2023-03-04 12:14:51 +0300

    UsersRepo next

--- app/src/main/java/com/girogevoro/gitapp/ui/users/MainActivity.kt
+++ app/src/main/java/com/girogevoro/gitapp/ui/users/MainActivity.kt
@@ -2,11 +2,26 @@ package com.girogevoro.gitapp.ui.users
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.girogevoro.gitapp.R
+import com.girogevoro.gitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.usersRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
